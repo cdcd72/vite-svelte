c84fd51ce0c587fded7947c7fbefd303e56d5cea
cdcd72 <cdcd71517@gmail.com>
2026-04-02 12:56:55 +0800

    feat: integrate Tailwind CSS and update project configurations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,7 @@
-<div class="container">
-    <h1>Hello Svelte!</h1>
+<div class="m-16 flex flex-col items-center justify-center bg-gray-50">
+    <h1
+        class=" text-6xl font-thin tracking-widest text-[#ff3e00] uppercase transition-colors duration-300 hover:text-[#ff5e00]"
+    >
+        Hello Svelte!
+    </h1>
 </div>
-
-<style>
-    .container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        font-size: 4em;
-        font-weight: 100;
-    }
-</style>
